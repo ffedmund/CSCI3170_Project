f185d4e67e2a4b55c87bda34b687ad668c909e99
antony6212 <106349561+antony6212@users.noreply.github.com>
2023-04-05 16:51:19 +0800

    Update Main.java
    done half of the connection part

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
+// cmd: java -cp .\mysql-connector-j-8.0.32\mysql-connector-j-8.0.32.jar;. Main
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.io.Console;
+import java.sql.*;
 
 public class Main {
-    static boolean logined = false;
+    // global var
+    static boolean connected = false;
+    static Connection conn = null;
+    static String host, dbname, username, password;
     static final String[][] content = {
         // page 0 (main menu)
-        {"Welcome to Book Ordering Management System",  // Title
+        {"===== Welcome to Book Ordering Management System =====",  // Title
         "Database Initialization",  // choice 1
         "Customer Operation",   // choice 2 ...
         "Bookstore Operation",
         "Quit"},
 
         // page 1 (Database Initialization)
-        {"         Database Initialization         ",
+        {"============== Database Initialization ==============",
         "Login to Database and Create Missing Tables",
         "Load from File",
         "Delete All Records",
@@ -20,7 +26,7 @@ public class Main {
         "Quit"},
 
         // page 2 (Customer Operation)
-        {"            Customer Operation            ",
+        {"================= Customer Operation =================",
         "Book Search",
         "Place Order",  // (make order)
         "Check History Orders",
@@ -28,35 +34,111 @@ public class Main {
         "Quit"},
 
         // page 3 (Bookstore Operation)
-        {"           Bookstore Operation           ",
+        {"================ Bookstore Operation ================",
         "Order Update",
         "Order Query",
         "Check Most Popular Books",
         "Back to Menu",
         "Quit"}
     };
-    static void showMenu(int page){
-        // clear screen
+    
+    // functions
+    static void clrscr(){   // clear screen
         for(int i = 0; i<100; i++)
             System.out.println();
+    }
+    static void showMenu(int page){
+        clrscr();
         
-        System.out.println("===== " + content[page][0] + " =====");
-        System.out.println(" + System Date: " + LocalDate.now());
+        System.out.println(content[page][0]);
+        System.out.println("\n + System Date: " + LocalDate.now());
         System.out.print(" + Database Records: ");
-        if(logined){
+        if(connected){
             // TODO: count record number
             System.out.println("...");
         }else{
-            System.out.println("--Please login to the database--");
+            System.out.println("--Fail to Connect to Database--");
         }
-        System.out.println("----------------------------------------------------");
-        for(int i = 1; i<content[page].length; i++){
-            System.out.println("> " + i + ". " + content[page][i]);
+        System.out.println("\n----------------------------------------------------\n");
+        for(int i = 1; i<8; i++){
+            if(i<content[page].length)
+                System.out.println("> " + i + ". " + content[page][i]);
+            else
+                System.out.println();
         }
         System.out.println();
     }
 
-    public static void main(String[] args) {
+    static void connectDatabase(){
+        clrscr();
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("Loading MySQL JDBC Driver...");
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(Exception x){
+            System.err.println("\nError: Unable to load the driver class!");
+            System.err.println("(Press Enter to continue)");
+            myScanner.nextLine();
+            return;
+        }
+        System.out.print("Enter your host server address(press Enter for local host): ");
+        host = myScanner.nextLine();
+        if(host == "")
+            host = "localhost";
+
+        System.out.print("Enter your Database Name(press Enter for defalut name: BOOKORDING)(create if not exist): ");
+        dbname = myScanner.nextLine();
+        if(dbname == "")
+            dbname = "BOOKORDING";
+
+        System.out.print("Enter your user name: ");
+        username = myScanner.nextLine();
+                
+        Console console = System.console();
+        if (console == null) {
+            System.out.print("Enter your password: ");
+            password = myScanner.nextLine();
+        }else{
+            password = new String(console.readPassword("Enter your password(masking): "));
+        }
+
+        // finish reading, try connection
+        System.out.println("Connecting to MySQL Database server...");
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://" + host, username, password);
+        }catch(Exception x){
+            System.err.println("\nError: Fail to connect to host server / Invalid username and password");
+            System.err.println("(Press Enter to continue)");
+            myScanner.nextLine();
+            return;
+        }
+
+        // try "use dbname" to switch database
+        try{
+            conn.setCatalog(dbname);
+        }catch(Exception x){
+            try{    // try to create database if not exist
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate("CREATE DATABASE " + dbname);
+                conn.setCatalog(dbname);
+            }catch(Exception y){
+                System.err.println("\nError: Fail to create database");
+                System.err.println("(Press Enter to continue)");
+                myScanner.nextLine();
+                return;
+            }
+        }
+
+        //TODO: try create table
+
+        System.out.println("");
+        myScanner.nextLine();
+
+        //TODO
+    }
+
+    public static void main(String[] args){
         int page = 0;
         int error = 0;
         while(page != 4){
@@ -102,6 +184,7 @@ public class Main {
                     switch(input){
                         case 1: // Login to Database and Create Missing Tables
                             // TODO
+                            connectDatabase();
                             break;
                         case 2: // Load from File
                             // TODO
