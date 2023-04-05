7a91adcacb0374b1723abcc523c8e7f3b20d3a0e
antony6212 <106349561+antony6212@users.noreply.github.com>
2023-04-06 05:47:31 +0800

    Update Main.java
    added showRs function, can use it to show the resultset after query

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 import java.time.LocalDate;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.Console;
 import java.sql.*;
 
@@ -17,7 +19,7 @@ public class Main {
         "Database Initialization",  // choice 1
         "Customer Operation",   // choice 2 ...
         "Bookstore Operation",
-        "Quit"},
+        "Quit the System"},
 
         // page 1 (Database Initialization)
         {"============== Database Initialization ==============",
@@ -25,7 +27,7 @@ public class Main {
         "Load from File",
         "Delete All Records",
         "Back to Menu",
-        "Quit"},
+        "Quit the System"},
 
         // page 2 (Customer Operation)
         {"================= Customer Operation =================",
@@ -33,7 +35,7 @@ public class Main {
         "Place Order",  // (make order)
         "Check History Orders",
         "Back to Menu",
-        "Quit"},
+        "Quit the System"},
 
         // page 3 (Bookstore Operation)
         {"================ Bookstore Operation ================",
@@ -41,7 +43,7 @@ public class Main {
         "Order Query",
         "Check Most Popular Books",
         "Back to Menu",
-        "Quit"}
+        "Quit the System"}
     };
     
     // functions
@@ -158,31 +160,31 @@ public class Main {
 
         HashMap<String, String> tableStruct = new HashMap<String, String>();
         tableStruct.put("Book", "(ISBN VARCHAR(255) not NULL, " +   //TODO: change to correct schema
-                                " Title VARCHAR(255), " + 
-                                " Authors VARCHAR(255), " + 
-                                " Price INTEGER, " + 
-                                " InventoryQuantity INTEGER, " +
-                                " PRIMARY KEY ( ISBN ))");
+                                    " Title VARCHAR(255), " + 
+                                    " Authors VARCHAR(255), " + 
+                                    " Price INTEGER, " + 
+                                    " InventoryQuantity INTEGER, " +
+                                    " PRIMARY KEY ( ISBN ))");
         tableStruct.put("Customer", "(ISBN VARCHAR(255) not NULL, " +   //TODO: change to correct schema
-                                    " first VARCHAR(255), " + 
-                                    " last VARCHAR(255), " + 
-                                    " age INTEGER, " + 
-                                    " PRIMARY KEY ( ISBN ))");
+                                        " first VARCHAR(255), " + 
+                                        " last VARCHAR(255), " + 
+                                        " age INTEGER, " + 
+                                        " PRIMARY KEY ( ISBN ))");
         tableStruct.put("Ordering", "(ISBN VARCHAR(255) not NULL, " +   //TODO: change to correct schema
-                                    " first VARCHAR(255), " + 
-                                    " last VARCHAR(255), " + 
-                                    " age INTEGER, " + 
-                                    " PRIMARY KEY ( ISBN ))");
+                                        " first VARCHAR(255), " + 
+                                        " last VARCHAR(255), " + 
+                                        " age INTEGER, " + 
+                                        " PRIMARY KEY ( ISBN ))");
         tableStruct.put("Cart", "(ISBN VARCHAR(255) not NULL, " +   //TODO: change to correct schema
-                                " first VARCHAR(255), " + 
-                                " last VARCHAR(255), " + 
-                                " age INTEGER, " + 
-                                " PRIMARY KEY ( ISBN ))");
-        tableStruct.put("Package", "(ISBN VARCHAR(255) not NULL, " +   //TODO: change to correct schema
                                     " first VARCHAR(255), " + 
                                     " last VARCHAR(255), " + 
                                     " age INTEGER, " + 
                                     " PRIMARY KEY ( ISBN ))");
+        tableStruct.put("Package", "(ISBN VARCHAR(255) not NULL, " +   //TODO: change to correct schema
+                                       " first VARCHAR(255), " + 
+                                       " last VARCHAR(255), " + 
+                                       " age INTEGER, " + 
+                                       " PRIMARY KEY ( ISBN ))");
         
         String newName;
         boolean newNameOK;
@@ -245,9 +247,225 @@ public class Main {
         connected = true;
     }
 
+    // try{
+    //     Statement stmt = conn.createStatement();
+    //     ResultSet rs = stmt.executeQuery("SELECT * FROM Book");
+    //     int[] colW = {4,7,4,2};
+    //     showRs(rs, "tututu title test", colW);
+    // }catch(Exception e){
+    //     e.printStackTrace();
+    //     System.err.println(e);
+    // }
+    /* colW e.g.:{6, 5, 3}
+     * +------+-----+---+
+     * quit with 0:normal, 1:error, 2:back to menu, 3:quit program */
+    static int showRs(ResultSet rs, String tiltle, int[] colW){
+        try{
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            ArrayList<String> colName = new ArrayList<String>();
+            List<ArrayList<String>> buffer = new ArrayList<ArrayList<String>>();
+            for (int i = 1; i <= columnsNumber; i++) {
+                colName.add(rsmd.getColumnName(i));
+            }
+            buffer.add(colName);
+            while (rs.next()) {
+                ArrayList<String> record = new ArrayList<>();
+                for (int i = 1; i <= columnsNumber; i++) {
+                    record.add(rs.getString(i));
+                }
+                buffer.add(record);
+            }
+            
+            if(colW.length != columnsNumber){
+                colW = new int[colName.size()];
+                for (int i = 0; i < colName.size(); i++) {
+                    colW[i] = colName.get(i).length();
+                    for (ArrayList<String> row : buffer) {
+                        if (row.get(i).length() > colW[i]) {
+                            colW[i] = row.get(i).length();
+                        }
+                    }
+                }
+                int sum = 0;
+                for(int w: colW)
+                    sum += w;
+                int min = 5;
+                while(sum+columnsNumber+1>80){
+                    boolean sumChanged = false;
+                    for(int i=0; i<columnsNumber; i++)
+                        if(colW[i]>min){
+                            colW[i]--;
+                            sumChanged = true;
+                        }
+                    if(!sumChanged){
+                        min--;
+                        if(min==0){
+                            for(int i=0; i<columnsNumber; i++)
+                                colW[i] = 2;
+                            break;
+                        }
+                    }
+                    sum = 0;
+                    for(int w: colW)
+                        sum += w;
+                }
+            }
+
+            int page = 1;
+            final int numberOfOnePage = 10;
+            int maxPage = 1;
+            while(maxPage*numberOfOnePage < buffer.size()-1)
+                maxPage++;
+            int input = 0;
+            Scanner myScanner = new Scanner(System.in);
+
+            while(input != 4){
+                clrscr();
+                System.out.printf("%" + ((80-tiltle.length())/2>0?(80-tiltle.length())/2:0) + "s%s\n", "", tiltle);
+
+                // print upper +-------+--------+...
+                for(int w : colW)
+                    System.out.print("+" + "-".repeat(w));
+                System.out.println("+");
+
+                HashMap<Integer, String> remainingString = new HashMap<Integer, String>();
+                for(int r=0; r<=numberOfOnePage; r++){
+                    ArrayList<String> row;
+                    if(r==0){
+                        row = buffer.get(0);
+                    }else{
+                        if((page-1)*numberOfOnePage + r >= buffer.size())
+                            break;
+                        row = buffer.get((page-1)*numberOfOnePage + r);
+                    }
+                    for(int i=0; i<columnsNumber; i++){
+                        String tmpS = row.get(i);
+                        if(tmpS==null)
+                            tmpS = "";
+                        if(tmpS.length()>colW[i]){
+                            char[] sp = {' ', ',', '-'};
+                            int[] pos = new int[sp.length];
+                            for(int j=0; j<sp.length; j++)
+                                pos[j] = tmpS.lastIndexOf(sp[j]);
+                            int maxIndex = -1;
+                            for (int p : pos) {
+                                if (p > maxIndex && p < colW[i]) {
+                                    maxIndex = p;
+                                }
+                            }
+                            if(maxIndex == -1)
+                                maxIndex = colW[i];
+                            else
+                                maxIndex++;
+                            remainingString.put(i, tmpS.substring(maxIndex));
+                            tmpS = tmpS.substring(0, maxIndex);
+                        }
+                        System.out.print("|" + tmpS + " ".repeat(colW[i]-tmpS.length()));
+                    }
+                    System.out.println("|");
+                    while(!remainingString.isEmpty()){
+                        for(int i=0; i<columnsNumber; i++){
+                            if(!remainingString.containsKey(i)){
+                                System.out.print("|" + " ".repeat(colW[i]));
+                                continue;
+                            }
+                            String tmpS = remainingString.get(i);
+                            remainingString.remove(i);
+                            if(tmpS.length()>colW[i]){
+                                char[] sp = {' ', ',', '-'};
+                                int[] pos = new int[sp.length];
+                                for(int j=0; j<sp.length; j++)
+                                    pos[j] = tmpS.lastIndexOf(sp[j]);
+                                int maxIndex = -1;
+                                for (int p : pos) {
+                                    if (p > maxIndex && p < colW[i]) {
+                                        maxIndex = p;
+                                    }
+                                }
+                                if(maxIndex == -1)
+                                    maxIndex = colW[i];
+                                else
+                                    maxIndex++;
+                                remainingString.put(i, tmpS.substring(maxIndex));
+                                tmpS = tmpS.substring(0, maxIndex);
+                            }
+                            System.out.print("|" + tmpS + " ".repeat(colW[i]-tmpS.length()));
+                        }
+                        System.out.println("|");
+                    }
+                    if(r==0){   // print middle +-------+--------+...
+                        for(int w : colW)
+                            System.out.print("+" + "-".repeat(w));
+                        System.out.println("+");
+                    }
+                }
+                // print bottom +-------+--------+...
+                for(int w : colW)
+                    System.out.print("+" + "-".repeat(w));
+                System.out.println("+\n");
+
+                String pageS = "Page " + page + "/" + maxPage;
+                System.out.print(page>1?"1. Previous Page  ":" ".repeat(18)); 
+                System.out.print(" ".repeat(22-(pageS.length()+1)/2>0?22-(pageS.length()+1)/2:0));
+                System.out.print(pageS + " ".repeat(22-pageS.length()/2>0?22-pageS.length()/2:0));
+                System.out.println(page<maxPage?"  3. Next Page":" ");
+                System.out.println("4. Finish" + " ".repeat(23) + "5. Back to Menu" + " ".repeat(17) + "6. Quit the System\n");
+                
+                switch(input){
+                    case 0:
+                        System.out.print(">>> Please Enter Your Query: ");
+                        break;
+                    case 2:
+                        System.out.print(">>> Input Error. Please Query from above Options: ");
+                        input = 0;
+                        break;
+                    case -1:
+                        System.out.print(">>> Input Error. Please Enter an integer Query: ");
+                        input = 0;
+                        break;
+                }
+                try{
+                    input = myScanner.nextInt();
+                }catch(Exception e){
+                    input = -1;
+                    continue;
+                }
+                switch(input){
+                    case 1:
+                        input = page>1 ? 0 : 2;
+                        if(page>1)
+                            page--;
+                        break;
+                    case 3:
+                        input = page<maxPage ? 0 : 2;
+                        if(page<maxPage)
+                            page++;
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        return 2;
+                    case 6:
+                        return 3;
+                    default:
+                        input = 2;
+                }
+            }
+        }catch(Exception x){
+            x.printStackTrace();
+            System.err.println(x);
+            Scanner myScanner = new Scanner(System.in);
+            myScanner.nextLine();
+            return 1;
+        }
+        return 0;
+    }
+
     public static void main(String[] args){
         int page = 0;
         int error = 0;
+        Scanner myScanner = new Scanner(System.in);
         while(page != 4){
             showMenu(page);
 
@@ -266,7 +484,6 @@ public class Main {
                     break;
             }
             System.out.print(">>> Please Enter Your Query: ");
-            Scanner myScanner = new Scanner(System.in);
             int input = -1;
             try{
                 input = myScanner.nextInt();
@@ -354,6 +571,7 @@ public class Main {
             }
 
         }
+        myScanner.close();
         System.out.println("Bye");
     }
 }
